a0627294b5c4503adb42248d89f2f53e78ff2517
Gisiger Mattia BIT <mattia.gisiger@bit.admin.ch>
2025-03-05 10:08:45 +0100

    added bookDTO

---
+++ BookTracker.Core/DTOs/BookDTO.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookTracker.Core.DTOs;
+
+public class BookDTO
+{
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(100)]
+	public string Title { get; set; }
+
+	[Required]
+	[MaxLength(100)]
+	public string Author { get; set; }
+
+	[MaxLength(1000)]
+	public string Description { get; set; }
+}
